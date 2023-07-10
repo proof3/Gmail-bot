c04de930566eb816dc4d46e09b599211bebfb02b
proof3 <124drak@gmail.com>
2023-07-10 18:30:23 +0400

    Check if messages have been replied and send reply

--- index.js
+++ index.js
@@ -3,9 +3,13 @@ import path from 'path';
 import process from 'process';
 import {authenticate} from '@google-cloud/local-auth';
 import {google} from 'googleapis';
+import { get } from 'http';
 
 // If modifying these scopes, delete token.json.
-const SCOPES = ['https://www.googleapis.com/auth/gmail.modify'];
+const SCOPES = ['https://mail.google.com/',
+    'https://www.googleapis.com/auth/gmail.modify',
+    'https://www.googleapis.com/auth/gmail.compose',
+    'https://www.googleapis.com/auth/gmail.send',];
 // The file token.json stores the user's access and refresh tokens, and is
 // created automatically when the authorization flow completes for the first
 // time.
@@ -15,6 +19,7 @@ const CREDENTIALS_PATH = path.join(process.cwd(), 'credentials.json');
 // timer for calling watchGmail
 let timer;
 let lastHistoryId;
+const respondedThreads = new Set();
 
 /**
  * Reads previously authorized credentials from the save file.
@@ -75,47 +80,30 @@ async function authorize() {
  */
 async function watchGmail(auth) {
     
+    //login in to gmail and get user profile to updat historyId
     const gmail = google.gmail({version: 'v1', auth});
     const profile = await gmail.users.getProfile({ userId: 'me' });
     
+    //check if historyId is initialized
     if (lastHistoryId) {
 
+        //retreive all messages added since last sync
         const res = await gmail.users.history.list({
             userId: 'me',
             startHistoryId: lastHistoryId,
             historyTypes: ['messageAdded'],
+            format: 'raw',
         });
 
         const history =  res.data.history;
-
+        
+        //check if there are any history records
         if (history && history.length > 0) {
-            console.log(history);
 
-            for (const {message} of history[history.length-1].messagesAdded) {
-
-                if (message) {
-                    console.log(message.id);
-                    const id = message.id;
-
-                    try {
-                        const content = await gmail.users.messages.get({
-                            userId: 'me',
-                            id: id,
-                        });
-
-                        console.log(content);
-
-                    } catch (e) {
-
-                        console.error('DID NOT GET MESSAGE', {
-                            messageId: id,
-                            error: e.message,
-                        });
-                    }
-                }
-            }
+            await processMessages(history[history.length-1].messagesAdded, gmail);
         }
     }
+
 
     lastHistoryId = profile.data.historyId;
     console.log(lastHistoryId);
@@ -123,6 +111,165 @@ async function watchGmail(auth) {
     const timeoutValue = Math.random() * (120-45) + 45;
     console.log(timeoutValue);
     timer = setTimeout(() => watchGmail(auth), 15 * 1000);
+}
+
+
+/** function checks if you have previously replied to thread
+ *  before turning the bot on */
+// async function checkIfReplied(gmail, messagesToReply, history) {
+
+//     if (history && history.length > 0) {
+//         console.log(history);
+
+//         //check if replied to thread
+//         for (const {message} of history[history.length-1].messagesAdded) {
+//             const {threadId} = message.threadId;
+//             if (respondedThreads.has(threadId)) {
+//                 continue;
+//             }
+
+//             try {
+//                 const thread = await gmail.users.threads.get({
+//                     userId: 'me',
+//                     id: threadId,
+//                 });
+//                 const {messages} = thread.data;
+
+//                 for(const {payload} of messages) {
+
+//                     const {headers} = payload.headers;
+//                     for(const {name, value} of headers) {
+
+//                         if (name === 'From' && value === 'me') {
+//                             respondedThreads.add(threadId);
+//                             break;
+//                         }
+//                         else if (name === 'From' && value !== 'me') {
+//                             messagesToReply.push(threadId);
+//                             break;
+//                         }
+//                     }
+//                 }
+//             }
+//             catch (e) {
+
+//                 console.error('DID NOT GET THREAD', {
+//                     threadId,
+//                     error: e.message,
+//                 });
+//             }
+//         }
+//     }
+// }
+
+async function processMessages(history, gmail) {
+
+    for (const {message} of history) {
+
+        if (message) {
+
+            const threadId = message.threadId;
+            const id = message.id;
+
+            //check if replied to thread
+            if (respondedThreads.has(threadId)) {
+                console.log('already replied to thread');
+                continue;
+            }
+
+            try {
+
+                const content = await gmail.users.messages.get({
+                    userId: 'me',
+                    id: id,
+                });
+
+                //parse headers to get from, to, subject, references, inReplyTo.
+                const headers = content.data.payload.headers;
+                let from, to, subject, references, inReplyTo;
+                console.log(headers);
+
+                for (const header of headers) {
+
+                    switch (header.name) {
+
+                        case 'From':
+                            to = header.value;
+                            break;
+                        case 'To':
+                            from = header.value;
+                            break;
+                        case 'Subject':
+                            subject = header.value;
+                            break;
+                        case 'Message-ID':
+                            references = header.value;
+                            inReplyTo = header.value;
+                            break;
+                        default:
+                            break;
+                    }
+                }
+                
+                const newThreadReply = await replyToMessage(gmail, from, to, subject, references, inReplyTo);
+                newThreadReply !== '' ? respondedThreads.add(newThreadReply): null;
+
+            } catch (e) {
+
+                console.error('DID NOT GET MESSAGE', {
+                    messageId: id,
+                    error: e.message,
+                });
+            }
+        }
+    }
+}
+
+async function replyToMessage(gmail, from, to, subject, references, inReplyTo) {
+
+    console.log('to: '+ to);
+
+    const messageParts = [
+        `From: ${from}`,
+        `To: ${to}`,
+        'Content-Type: text/html; charset=utf-8',
+        'MIME-Version: 1.0',
+        `Subject: Re: ${subject}`,
+        `References: ${references}`,
+        `In-Reply-To: ${inReplyTo}`,
+        '',
+        'This is a message just to say hello.',
+        'So... <b>Hello!</b>  🤘❤️😎',
+    ];
+    const message = messageParts.join('\n');
+    
+    // The body needs to be base64url encoded.
+    const encodedMessage = Buffer.from(message)
+        .toString('base64')
+        .replace(/\+/g, '-')
+        .replace(/\//g, '_')
+        .replace(/=+$/, '');
+
+    try {
+
+        const res = await gmail.users.messages.send({
+            userId: 'me',
+            requestBody: {
+              raw: encodedMessage,
+            },
+          });
+
+        console.log(res.data);
+        return res.data.threadId;
+    }
+    catch (e) {
+
+        console.error('DID NOT SEND MESSAGE', {
+            error: e.message,
+        });
+    }
+
+    return '';
 }
 
 authorize().then(watchGmail).catch(console.error);
